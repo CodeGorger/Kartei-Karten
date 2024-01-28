7f8353f082de05176fa73ccee9932dd9c2ba2c39
Simon Poschenrieder <simon.poschenrieder@gmail.com>
2024-01-28 21:07:22 +0100

    bugfix & example

--- SoundWrapper.cs
+++ SoundWrapper.cs
@@ -12,28 +12,27 @@ namespace KarteiKartenLernen
 {
     class SoundWrapper
     {
-        public SoundWrapper(string sound_file)
+        public SoundWrapper(string in_sound_file)
         {
-            if (sound_file.EndsWith(".wav"))
-            {
-                wav_player = new SoundPlayer(sound_file);
-            }
-            else if (sound_file.EndsWith(".mp3"))
-            {
-                Mp3FileReader mp3File = new Mp3FileReader(sound_file);
-                mp3_player = new WaveOutEvent();
-                mp3_player.Init(mp3File);
-            }
+            sound_file = in_sound_file;
         }
 
         public void Play()
         {
-            wav_player?.Play();
-            mp3_player?.Play();
+            if (sound_file.EndsWith(".wav"))
+            {
+                SoundPlayer wav_player = new SoundPlayer(sound_file);
+                wav_player.Play();
+            }
+            else if (sound_file.EndsWith(".mp3"))
+            {
+                Mp3FileReader mp3File = new Mp3FileReader(sound_file);
+                WaveOutEvent mp3_player = new WaveOutEvent();
+                mp3_player.Init(mp3File);
+                mp3_player.Play();
+            }
         }
 
-        private SoundPlayer wav_player = null;
-        private WaveOutEvent mp3_player = null;
-
+        private string sound_file;
     }
 }
